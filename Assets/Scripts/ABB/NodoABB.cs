cafc5bfb4c2829fae8a03f4311e75126a4bd1e14
WishedClover48 <matuleon2001@gmail.com>
2024-11-14 16:14:14 -0300

    Added TDAabb
    Added the scripts for the TDAABB

---
+++ Assets/Scripts/ABB/NodoABB.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodoABB
+{
+    // datos a almacenar, en este caso un entero
+    public int info;
+    // referencia los nodos izquiero y derecho
+    public ABBTDA hijoIzq;
+    public ABBTDA hijoDer;
+}
